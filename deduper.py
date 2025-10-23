0f8dbce0de95b4f7b1e696cfaad57c5ea99ec217
Catherine Carmosino <catcar@uoregon.edu>
2025-10-23 14:26:27 -0700

    created the UMI set

--- deduper.py
+++ deduper.py
@@ -5,10 +5,9 @@ import argparse
 def get_args():
 	'''this has 1 arguement: sam file'''
 	parser = argparse.ArgumentParser(description="Getting")
-	parser.add_argument("-f", help="absolute file path to sorted sam file", type=str, required=True)
-	parser.add_argument("-o", help="absolute file path to sorted sam file", type=str, required=True)
-	parser.add_argument("-u", help="absolute path to file containing the list of UMIs", type=str, required=True)
-	parser.add_argument("-h", help="prints a USEFUL help message and any assumptions your code", type=str, required=False)
+	parser.add_argument("-f", help="The absolute path to the sorted sam file", type=str, required=True)
+	parser.add_argument("-o", help="The absolute path to the sorted sam file", type=str, required=True)
+	parser.add_argument("-u", help="The absolute path to the file containing the list of UMIs", type=str, required=True)
 	return parser.parse_args()
 
 #calling the function (so in the terminal it runs)
@@ -23,5 +22,18 @@ o:str = args.o
 #reassiging args.sam filename as f
 u:str = args.u
 
-#reassiging args.output filename as o
-h:str = args.h
+
+#creating an empty set to store all UMI records
+UMI:set = set()
+
+#opening the UMI file to read
+with open(u, "r") as umi_file:
+	#looping through each UMI line
+    for line in umi_file:
+        #stripping the line!
+        line = line.strip("\n")
+        #adding the UMI string to the set
+        UMI.add(line)
+
+print(UMI)
+
